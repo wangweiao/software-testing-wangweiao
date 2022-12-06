a8fa5947b1d368aa9bd7c95bf1b652a7805dc467
Weiao Wang <weiao.wang@yahoo.com>
2022-12-06 21:36:29 +0100

    updated feature file.

---
+++ src/test/java/org/example/LogOutStepDefs.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LogOutStepDefs {
+    @Given("the homepage is opened")
+    public void theHomepageIsOpened() {
+    }
+
+    @And("the Username is filled with {string} and Password is filled with {string}")
+    public void theUsernameIsFilledWithUsernameAndPasswordIsFilledWithPassword() {
+    }
+
+    @And("the Login button is clicked")
+    public void theLoginButtonIsClicked() {
+    }
+
+    @And("the Menu button is clicked")
+    public void theMenuButtonIsClicked() {
+    }
+
+    @When("the Log out button is clicked")
+    public void theLogOutButtonIsClicked() {
+    }
+
+    @Then("the Log out should be successful")
+    public void theLogOutShouldBeSuccessful() {
+    }
+}
